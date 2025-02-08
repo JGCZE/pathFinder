c897c5655bb90b0bd10b48457b3b6940879c54d7
Jan Gross <jan.gross.cze@gmail.com>
2025-02-08 16:54:14 +0100

    More context

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { PathfindingProvider } from "./context/PathFindingContext";
+import { SpeedProvider } from "./context/SpeedContext";
+import { TileProvider } from "./context/TileContext";
 
 function App() {
   return (
     <PathfindingProvider>
-      <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-600">
-        Hello world!
-      </h1>
+      <TileProvider>
+        <SpeedProvider>
+          <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-600">
+            Hello world!
+          </h1>
+        </SpeedProvider>
+      </TileProvider>
     </PathfindingProvider>
   );
 }
